cc7c7f6d91f744aecdf4d5ade2fd7f489cfbcc8d
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-23 14:34:03 -0800

    Merge master

---
+++ button.css.ts
@@ -0,0 +1,76 @@
+import { recipe } from "@vanilla-extract/recipes";
+import { tokens } from "grommet-exp-tokens";
+
+export const button = recipe({
+  base: {
+    borderRadius: tokens[`radius-full`],
+    color: tokens[`palette-green-400`],
+    fontWeight: tokens[`font-weight-bold`],
+    fontSize: tokens[`font-size-text-medium`],
+    lineHeight: tokens[`font-height-text-medium`],
+    selectors: {
+      [`&:hover`]: {
+        cursor: `pointer`,
+      },
+    },
+  },
+
+  variants: {
+    kind: {
+      default: {},
+      secondary: {
+        background: `none`,
+        borderWidth: tokens[`stroke-weight-small`],
+        borderColor: tokens[`palette-green-400`],
+        borderStyle: `solid`,
+      },
+      primary: {
+        background: tokens[`palette-green-400`],
+        border: `none`,
+        color: `#FFF`,
+      },
+    },
+    size: {
+      small: {
+        paddingLeft: tokens[`spacing-medium`],
+        paddingRight: tokens[`spacing-medium`],
+        paddingTop: tokens[`spacing-xsmall`],
+        paddingBottom: tokens[`spacing-xsmall`],
+        fontSize: tokens[`font-size-text-small`],
+      },
+      medium: {
+        paddingLeft: tokens[`spacing-medium`],
+        paddingRight: tokens[`spacing-medium`],
+        paddingTop: tokens[`spacing-xsmall`],
+        paddingBottom: tokens[`spacing-xsmall`],
+      },
+      large: {
+        paddingLeft: tokens[`spacing-medium`],
+        paddingRight: tokens[`spacing-medium`],
+        paddingTop: tokens[`spacing-small`],
+        paddingBottom: tokens[`spacing-small`],
+      },
+    },
+    iconOnly: {
+      true: {},
+    },
+  },
+
+  // Applied when multiple variants are set at once
+  compoundVariants: [
+    {
+      variants: {
+        iconOnly: true,
+        size: `medium`,
+      },
+      style: {
+        padding: tokens[`spacing-xsmall`],
+      },
+    },
+  ],
+
+  defaultVariants: {
+    kind: `default`,
+    size: `medium`,
+  },
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from './anchor.css' ;
 export * from './box.css' ;
+export * from "./button.css";
 export * from './heading.css' ;
 export * from './text.css' ;
 export * from "./vars.css";
