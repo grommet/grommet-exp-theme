ec98f8a88796469c0366f9f89f65d8565434bad0
Taylor Seamans <taylor.seamans@yahoo.com>
2023-04-19 15:10:26 -0700

    Add css

---
+++ textInput.css.ts
@@ -0,0 +1,17 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "./theme.css";
+
+export const textInput = style({
+  boxSizing: "border-box",
+  fontFamily: "inherit",
+  fontSize: "inherit",
+  lineHeight: "inherit",
+  WebkitAppearance: "none",
+  background: "transparent",
+  color: vars.input.value.color,
+  fontWeight: vars.input.value.fontWeight,
+  width: "100%",
+  padding: `${vars.input.padding}`,
+  borderWidth: `${vars.input.borderSize.width}`,
+  borderColor: `${vars.input.borderColor.default}`,
+});
